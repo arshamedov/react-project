78542c77b93300a846d12e55c5af0d6820152ea8
Arsham <muradyanarsham311@gmail.com>
2024-06-06 23:31:42 +0400

    selecting event

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,50 +1,73 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../../context";
 import './style.css';
 
 const Events: React.FC = (): JSX.Element => {
-
     const { movies, theaters, concerts } = useContext(Context);
+    const [selectedItems, setSelectedItems] = useState<any[]>([]);
 
-    return <div>
-        <div className="event-category">
-            <h2>Movies</h2>
-            {movies?.map((elm) => {
-                return <div key={elm.id} className="event-item">
-                    <div>
-                        <img src={elm.img} />
-                        <h3>{elm.name}</h3>
-                        <p>{elm.category}</p>
-                    </div>
-                    <span>{elm.year}</span>
-                </div>
-            })}
-        </div>
+    const handleSelect = (item: Object) => {
+        setSelectedItems([...selectedItems, item]);
+    };
 
-        <div className="event-category">
-            <h2>Theaters</h2>
-            {theaters?.map((elm) => {
-                return <div key={elm.id} className="event-item">
-                    <div>
-                        <img src={elm.img}/>
-                        <h3>{elm.name}</h3>
-                    </div>
+    return (
+        <div className="events-container">
+            <div className="event-list">
+                <div className="event-category">
+                    <h2>Movies</h2>
+                    {movies?.map((elm) => (
+                        <div key={elm.id} className="event-item">
+                            <div>
+                                <img src={elm.img} alt={elm.name} />
+                                <h3>{elm.name}</h3>
+                                <p>{elm.category}</p>
+                            </div>
+                            <span>{elm.year}</span>
+                            <button onClick={() => handleSelect(elm)}>Select</button>
+                        </div>
+                    ))}
                 </div>
-            })}
-        </div>
 
-        <div className="event-category">
-            <h2>Concerts</h2>
-            {concerts?.map((elm) => {
-                return <div key={elm.id} className="event-item">
-                    <div>
-                        <img src={elm.img}/>
-                        <h3>{elm.name}</h3>
-                    </div>
+                <div className="event-category">
+                    <h2>Theaters</h2>
+                    {theaters?.map((elm) => (
+                        <div key={elm.id} className="event-item">
+                            <div>
+                                <img src={elm.img} alt={elm.name} />
+                                <h3>{elm.name}</h3>
+                            </div>
+                            <button onClick={() => handleSelect(elm)}>Select</button>
+                        </div>
+                    ))}
                 </div>
-            })}
+
+                <div className="event-category">
+                    <h2>Concerts</h2>
+                    {concerts?.map((elm) => (
+                        <div key={elm.id} className="event-item">
+                            <div>
+                                <img src={elm.img} alt={elm.name} />
+                                <h3>{elm.name}</h3>
+                            </div>
+                            <button onClick={() => handleSelect(elm)}>Select</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="selected-items">
+                <h2>Selected Events</h2>
+                {selectedItems.map((item, index) => (
+                    <div key={index} className="selected-item">
+                        <img src={item.img} alt={item.name} />
+                        <h3>{item.name}</h3>
+                        <p>{item.category}</p>
+                        <span>{item.year}</span>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-}
+    );
+};
 
 export default Events;
